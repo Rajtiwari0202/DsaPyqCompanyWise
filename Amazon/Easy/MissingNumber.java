c41d8b897763772eab4c24ea050ccd9338b33f42
Raj Tiwari <rajtiwari16916@gmail.com>
2026-05-27 16:18:34 +0000

    Add Missing Number XOR solution in Java

---
+++ Amazon/Easy/MissingNumber.java
@@ -0,0 +1,18 @@
+class MissingNumber{
+    public static void main(String[] args){
+        int nums[] = new int[3];
+        nums[0]=3;
+        nums[1]=0;
+        nums[2]=1;
+
+        System.out.println(MissingNumber(nums));
+    }
+    public static int MissingNumber(int[] nums) {
+        int n_xor=nums.length;
+        for(int i=0;i<nums.length;i++){
+            n_xor=n_xor^i;
+            n_xor = n_xor ^nums[i];
+        }
+        return n_xor;
+    }
+}
