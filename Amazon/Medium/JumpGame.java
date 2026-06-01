afbbf14b48621d023d5bc559ac87bddf575abc50
Rajtiwari0202 <rajtiwari16916@gmail.com>
2026-06-01 10:48:44 +0530

    Add Jump Game greedy solution in Java

--- Amazon/Medium/JumpGame.java
+++ Amazon/Medium/JumpGame.java
@@ -1,43 +1,64 @@
-import java.util.*;
+// import java.util.*;
 
-public class JumpGame {
+// public class JumpGame {
 
-    static HashMap<Integer, Boolean> memo;
+//     static HashMap<Integer, Boolean> memo;
 
-    static boolean helper(int[] nums, int i) {
+//     static boolean helper(int[] nums, int i) {
 
-        if (i >= nums.length - 1) return true;
+//         if (i >= nums.length - 1) return true;
 
-        if (nums[i] == 0) return false;
+//         if (nums[i] == 0) return false;
 
-        if (memo.containsKey(i)) {
-            return memo.get(i);
-        }
+//         if (memo.containsKey(i)) {
+//             return memo.get(i);
+//         }
 
-        for (int j = 1; j <= nums[i]; j++) {
+//         for (int j = 1; j <= nums[i]; j++) {
 
-            if (helper(nums, i + j)) {
-                memo.put(i, true);
-                return true;
+//             if (helper(nums, i + j)) {
+//                 memo.put(i, true);
+//                 return true;
+//             }
+//         }
+
+//         memo.put(i, false);
+
+//         return false;
+//     }
+
+//     static boolean canJump(int[] nums) {
+
+//         memo = new HashMap<>();
+
+//         return helper(nums, 0);
+//     }
+
+//     public static void main(String[] args) {
+
+//         int[] nums = {2,3,1,1,4};
+
+//         System.out.println(canJump(nums));
+//     }
+// }
+
+// Optimal Greedy Solution (Interview Preferred)
+class Solution {
+
+    public boolean canJump(int[] nums) {
+
+        int maxReach = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            // Cannot reach this index
+            if (i > maxReach) {
+                return false;
             }
+
+            maxReach = Math.max(maxReach, i + nums[i]);
         }
 
-        memo.put(i, false);
-
-        return false;
-    }
-
-    static boolean canJump(int[] nums) {
-
-        memo = new HashMap<>();
-
-        return helper(nums, 0);
-    }
-
-    public static void main(String[] args) {
-
-        int[] nums = {2,3,1,1,4};
-
-        System.out.println(canJump(nums));
+        return true;
     }
 }
